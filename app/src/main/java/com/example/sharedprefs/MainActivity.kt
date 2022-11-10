03d41d447bc47e2053d966fec1a0fbfa2096da65
mahabub.pasha <mahbub.pasha@randomaccessinc.com>
2022-11-10 23:46:51 +0600

    buil bmi calculator using native android-kotlin

--- app/src/main/java/com/example/sharedprefs/MainActivity.kt
+++ app/src/main/java/com/example/sharedprefs/MainActivity.kt
@@ -6,40 +6,10 @@ import android.os.Bundle
 import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var nameText:EditText
-    private lateinit var ageText:EditText
-    private lateinit var sf:SharedPreferences
-    private lateinit var editor:SharedPreferences.Editor
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        nameText = findViewById(R.id.etName)
-        ageText = findViewById(R.id.etAge)
-        sf = getSharedPreferences("my_sf", MODE_PRIVATE)
-        editor = sf.edit()
-    }
-
-    override fun onPause() {
-        super.onPause()
-        val name = nameText.text.toString()
-        val age = Integer.parseInt(ageText.text.toString())
-        editor.putString("sf_name", name)
-        editor.putInt("sf_age", age)
-        editor.apply()
-        editor.commit()
-    }
-
-    override fun onResume() {
-        super.onResume()
-        val name = sf.getString("sf_name", null)
-        val age = sf.getInt("sf_age",0)
-        nameText.setText(name)
-        if(age != 0) {
-            ageText.setText(age.toString())
-        }
-
     }
 }
 
